298884330cd19c192b235dbc5202af1b182c642b
dbnorth <david.north@oc.edu>
2025-05-27 22:08:36 -0500

    add OCR fuinction

--- src/services/uploadServices.js
+++ src/services/uploadServices.js
@@ -4,11 +4,11 @@ export default {
   uploadTranscript(file, transcriptId) {
     console.log('Uploading file for transcript:', transcriptId);
     const formData = new FormData();
-    formData.append("transcriptId", transcriptId.toString());
     formData.append("file", file);
+    formData.append("transcriptId", transcriptId);
     formData.append("filename", `transcript-${transcriptId}.pdf`);
 
-    return apiClient.post("/upload", formData, {
+    return apiClient.post("/transcript/upload", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
         "Accept": "application/json",
@@ -16,4 +16,8 @@ export default {
       transformRequest: [(data) => data],
     });
   },
+
+  processOCR(transcriptId) {
+    return apiClient.get(`/transcript/ocr/${transcriptId}`);
+  }
 }; 
